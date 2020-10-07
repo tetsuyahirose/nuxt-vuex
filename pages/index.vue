9a88c7cf22537fbf39ca34b4a951a069fb774caa
tetsuyahirose <tetsuya19990510gmail.com>
2020-10-07 13:50:37 +0900

    Vuexについて学んだ

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="container">
+    <div>
+      <!-- <p>{{ $store.state.message }}</p> -->
+      <p>{{ $store.state.hello.message }}</p>
+      <!-- <button
+        v-on:click="$store.commit('updateMessage', 'Commit with payload')"
+      >
+        Updated
+      </button> -->
+      <button
+        v-on:click="
+          $store.dispatch('hello/updateMessageAction', 'Dispatch with payload')
+        "
+      >
+        Dispatch
+      </button>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+import VuetifyLogo from "~/components/VuetifyLogo.vue";
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo,
+  },
+};
+</script>
